8a61d24d63bc1d668a2c0a90cf09855634df65cd
Omprakash Sridharan <omprakashsridharan@outlook.com>
2022-05-17 10:44:12 +0200

    Adds sqlx load query impl

--- thalo-sqlx/src/event_store.rs
+++ thalo-sqlx/src/event_store.rs
@@ -1,24 +1,97 @@
-use sqlx::{error::Error, pool::Pool, Connection, Database};
+use async_trait::async_trait;
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use sqlx::{
+    any::{Any, AnyConnectOptions},
+    error::Error,
+    pool::Pool,
+    types::chrono::{DateTime, FixedOffset},
+    Decode, FromRow,
+};
+use thalo::{
+    aggregate::{Aggregate, TypeId},
+    event::AggregateEventEnvelope,
+    event_store::EventStore,
+};
+
+const LOAD_EVENTS_QUERY: &str = include_str!("queries/load_events.sql");
 
 #[derive(Clone)]
-pub struct SqlEventStore<DB>
-where
-    DB: Database,
-{
-    pool: Pool<DB>,
+pub struct SqlEventStore {
+    pool: Pool<Any>,
 }
 
-impl<DB> SqlEventStore<DB>
-where
-    DB: Database,
-{
-    pub async fn connect(
-        connect_options: <<DB as Database>::Connection as Connection>::Options,
-    ) -> Result<Self, Error> {
-        let x: Result<Pool<DB>, Error> = Pool::connect_with(connect_options).await;
+impl SqlEventStore {
+    pub async fn connect(connect_options: AnyConnectOptions) -> Result<Self, Error> {
+        let x = Pool::connect_with(connect_options).await;
         match x {
             Ok(pool) => Ok(Self { pool }),
             Err(e) => Err(e),
         }
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize, FromRow, Decode)]
+struct LoadEventsRow {
+    pub id: i64,
+    #[sqlx]
+    pub created_at: DateTime<FixedOffset>,
+    pub aggregate_type: String,
+    pub aggregate_id: String,
+    pub sequence: i64,
+    pub event_data: String,
+}
+
+#[async_trait]
+impl EventStore for SqlEventStore {
+    type Error = i32;
+
+    async fn load_events<A>(
+        &self,
+        id: Option<&<A as Aggregate>::ID>,
+    ) -> Result<Vec<AggregateEventEnvelope<A>>, Self::Error>
+    where
+        A: Aggregate,
+        <A as Aggregate>::Event: DeserializeOwned,
+    {
+        let row: Vec<LoadEventsRow> = sqlx::query_as("src/queries/load_events.sql")
+            .bind(&<A as TypeId>::type_id())
+            .bind(&id.map(|id| id.to_string()))
+            .fetch_all(&self.pool)
+            .await
+            .unwrap();
+        todo!()
+    }
+
+    async fn load_events_by_id<A>(
+        &self,
+        ids: &[u64],
+    ) -> Result<Vec<AggregateEventEnvelope<A>>, Self::Error>
+    where
+        A: Aggregate,
+        <A as Aggregate>::Event: DeserializeOwned,
+    {
+        todo!()
+    }
+
+    async fn load_aggregate_sequence<A>(
+        &self,
+        id: &<A as Aggregate>::ID,
+    ) -> Result<Option<u64>, Self::Error>
+    where
+        A: Aggregate,
+    {
+        todo!()
+    }
+
+    async fn save_events<A>(
+        &self,
+        id: &<A as Aggregate>::ID,
+        events: &[<A as Aggregate>::Event],
+    ) -> Result<Vec<u64>, Self::Error>
+    where
+        A: Aggregate,
+        <A as Aggregate>::Event: Serialize,
+    {
+        todo!()
     }
 }
